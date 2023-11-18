a0a40cc8130ada01f9986a02dc57815de8f9490f
git config --global user.email piptr@protonmail.com <piptr@protonmail.com>
2023-11-18 00:27:06 -0300

    iniciando os trabalhos

---
+++ zabbix-6.4-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "##############################################################################"
+echo "#                                                                            #"
+echo "#           ______         ____  ____ _______   __    __ _  _                #"
+echo "#          |___  /   /\   |  _ \|  _ \_   _\ \ / /   / /| || |               #"
+echo "#             / /   /  \  | |_) | |_) || |  \ V /   / /_| || |_              #"
+echo "#            / /   / /\ \ |  _ <|  _ < | |   > <   | '_ \__   _|             #"
+echo "#           / /__ / ____ \| |_) | |_) || |_ / . \  | (_) | | |               #"
+echo "#          /_____/_/    \_\____/|____/_____/_/ \_\  \___(_)|_|               #"
+echo "#                                                                            #"
+echo "#                                                                            #"
+echo "##############################################################################"
+
+### PROJECT INFO
+echo "# project         : Monx Free Software Solutions"
+echo "# title           : zabbix-6.4-install.sh"
+echo "# description     : Script destinado à instalação do Zabbix Server & Zabbix Agent."
+echo "# date            : 20/11/23"
+echo "# version         : 6.4"
+echo "# usage           : bash zabbix-6.4-install.sh"
+echo "# contato			: Ranlens Denck <ranlens.denck@protonmail.com>
+echo "# ATENTION!		: EXECUTE TODO O BASH COMO ROOT
+### END PROJECT INFO
+
+
+
