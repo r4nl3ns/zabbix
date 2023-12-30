55672b2a7939e739a3cf5ab70a28e081264186a7
r4nl3ns <piptr@protonmail.com>
2023-12-30 12:24:48 -0300

    correção brach

--- Zabbix DB/postgresql-install.sh
+++ Zabbix DB/postgresql-install.sh
@@ -17,26 +17,26 @@ sudo dnf -y install zabbix-sql-scripts zabbix-agent
 echo "Instalação feita com sucesso!"
 #------------------------------------------Agora vamos instalar o Postgresql-14----------------------------------------------#
 # Verificar a lista de repositórios disponíveis para RHELL
-dnf module list postgresql
+sudo dnf module list postgresql
 
 # Instalação do banco de dados Postgresql 14
-dnf -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo dnf -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
 # Desativar o repositório padrão do PostgreSQL
-dnf -qy module disable postgresql 
+sudo dnf -qy module disable postgresql 
 
 # Instalar o PostgreSQL 14
-dnf -y install postgresql14 postgresql14-server 
+sudo dnf -y install postgresql14 postgresql14-server 
 
 # Inicializa o Postgresql
 /usr/pgsql-14/bin/postgresql-14-setup initdb
 
 # Inicia o postgresql
-systemctl start postgresql-14
+sudo systemctl start postgresql-14
 # Habilita o postgresql
-systemctl enable postgresql-14
+sudo systemctl enable postgresql-14
 # Verifica o status do postgresql
-systemctl status postgresql-14
+sudo systemctl status postgresql-14
 
 --
 # Criar usuario zabbix
